43eac5131b24483177b2798816ea7eff42c2dcf3
Abdulrahman Majash <the0dark@gmail.com>
2018-09-06 02:14:35 +0300

    Using io.open for Python 2 compatibility
    Using io.open instead of open since the it supports the encoding flag.

--- downloader.py
+++ downloader.py
@@ -9,6 +9,7 @@ import json
 import requests
 import argparse
 import lxml.html
+import io
 
 from lxml.cssselect import CSSSelector
 
@@ -147,7 +148,7 @@ def main(argv):
 
         print('Downloading Youtube comments for video:', youtube_id)
         count = 0
-        with open(output, 'w', encoding='utf8') as fp:
+        with io.open(output, 'w', encoding='utf8') as fp:
             for comment in download_comments(youtube_id):
                 print(json.dumps(comment, ensure_ascii=False), file=fp)
                 count += 1

4e88a52f4320472ad8bc18a0c794ac848c1c151b
Egbert Bouman <ebouman@gmail.com>
2020-12-07 15:01:37 +0100

    Fix issues due to Youtube update

--- downloader.py
+++ downloader.py
@@ -52,8 +52,9 @@ def download_comments_new_api(youtube_id, sleep=1):
     response = session.get(YOUTUBE_VIDEO_URL.format(youtube_id=youtube_id))
     html = response.text
     session_token = find_value(html, 'XSRF_TOKEN', 3)
+    session_token = bytes(session_token, 'ascii').decode('unicode-escape')
 
-    data = json.loads(find_value(html, 'window["ytInitialData"] = ', 0, '\n').rstrip(';'))
+    data = json.loads(find_value(html, 'var ytInitialData = ', 0, '};') + '}')
     for renderer in search_dict(data, 'itemSectionRenderer'):
         ncd = next(search_dict(renderer, 'nextContinuationData'), None)
         if ncd:
@@ -70,7 +71,7 @@ def download_comments_new_api(youtube_id, sleep=1):
                                         'itct': itct},
                                 data={'session_token': session_token},
                                 headers={'X-YouTube-Client-Name': '1',
-                                         'X-YouTube-Client-Version': '2.20200207.03.01'})
+                                         'X-YouTube-Client-Version': '2.20201202.06.01'})
 
         if not response:
             break
@@ -126,6 +127,7 @@ def download_comments_old_api(youtube_id, sleep=1):
 
     page_token = find_value(html, 'data-token')
     session_token = find_value(html, 'XSRF_TOKEN', 3)
+    session_token = bytes(session_token, 'ascii').decode('unicode-escape')
 
     first_iteration = True
 

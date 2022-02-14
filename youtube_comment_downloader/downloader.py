87c4f908b5c6cc0610c4d708cf5aa2b7a851d066
Egbert Bouman <ebouman@gmail.com>
2022-02-14 13:58:09 +0100

    Move code into a class and add time_parsed to the JSON output

--- youtube_comment_downloader/downloader.py
+++ youtube_comment_downloader/downloader.py
@@ -6,10 +6,11 @@ import argparse
 import io
 import json
 import os
+import re
 import sys
 import time
 
-import re
+import dateparser
 import requests
 
 YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v={youtube_id}'
@@ -23,108 +24,115 @@ YT_CFG_RE = r'ytcfg\.set\s*\(\s*({.+?})\s*\)\s*;'
 YT_INITIAL_DATA_RE = r'(?:window\s*\[\s*["\']ytInitialData["\']\s*\]|ytInitialData)\s*=\s*({.+?})\s*;\s*(?:var\s+meta|</script|\n)'
 
 
-def regex_search(text, pattern, group=1, default=None):
-    match = re.search(pattern, text)
-    return match.group(group) if match else default
+class YoutubeCommentDownloader:
 
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers['User-Agent'] = USER_AGENT
 
-def ajax_request(session, endpoint, ytcfg, retries=5, sleep=20):
-    url = 'https://www.youtube.com' + endpoint['commandMetadata']['webCommandMetadata']['apiUrl']
-    
-    data = {'context': ytcfg['INNERTUBE_CONTEXT'],
-            'continuation': endpoint['continuationCommand']['token']}
+    def ajax_request(self, endpoint, ytcfg, retries=5, sleep=20):
+        url = 'https://www.youtube.com' + endpoint['commandMetadata']['webCommandMetadata']['apiUrl']
 
-    for _ in range(retries):
-        response = session.post(url, params={'key': ytcfg['INNERTUBE_API_KEY']}, json=data)
-        if response.status_code == 200:
-            return response.json()
-        if response.status_code in [403, 413]:
-            return {}
-        else:
+        data = {'context': ytcfg['INNERTUBE_CONTEXT'],
+                'continuation': endpoint['continuationCommand']['token']}
+
+        for _ in range(retries):
+            response = self.session.post(url, params={'key': ytcfg['INNERTUBE_API_KEY']}, json=data)
+            if response.status_code == 200:
+                return response.json()
+            if response.status_code in [403, 413]:
+                return {}
+            else:
+                time.sleep(sleep)
+
+    def get_comments(self, youtube_id, sort_by=SORT_BY_RECENT, language=None, sleep=.1):
+        response = self.session.get(YOUTUBE_VIDEO_URL.format(youtube_id=youtube_id))
+
+        if 'uxe=' in response.request.url:
+            self.session.cookies.set('CONSENT', 'YES+cb', domain='.youtube.com')
+            response = self.session.get(YOUTUBE_VIDEO_URL.format(youtube_id=youtube_id))
+
+        html = response.text
+        ytcfg = json.loads(self.regex_search(html, YT_CFG_RE, default=''))
+        if not ytcfg:
+            return # Unable to extract configuration
+        if language:
+            ytcfg['INNERTUBE_CONTEXT']['client']['hl'] = language
+
+        data = json.loads(self.regex_search(html, YT_INITIAL_DATA_RE, default=''))
+
+        section = next(self.search_dict(data, 'itemSectionRenderer'), None)
+        renderer = next(self.search_dict(section, 'continuationItemRenderer'), None) if section else None
+        if not renderer:
+            # Comments disabled?
+            return
+
+        needs_sorting = sort_by != SORT_BY_POPULAR
+        continuations = [renderer['continuationEndpoint']]
+        while continuations:
+            continuation = continuations.pop()
+            response = self.ajax_request(continuation, ytcfg)
+
+            if not response:
+                break
+
+            error = next(self.search_dict(response, 'externalErrorMessage'), None)
+            if error:
+                raise RuntimeError('Error returned from server: ' + error)
+
+            if needs_sorting:
+                sort_menu = next(self.search_dict(response, 'sortFilterSubMenuRenderer'), {}).get('subMenuItems', [])
+                if sort_by < len(sort_menu):
+                    continuations = [sort_menu[sort_by]['serviceEndpoint']]
+                    needs_sorting = False
+                    continue
+                raise RuntimeError('Failed to set sorting')
+
+            actions = list(self.search_dict(response, 'reloadContinuationItemsCommand')) + \
+                      list(self.search_dict(response, 'appendContinuationItemsAction'))
+            for action in actions:
+                for item in action.get('continuationItems', []):
+                    if action['targetId'] == 'comments-section':
+                        # Process continuations for comments and replies.
+                        continuations[:0] = [ep for ep in self.search_dict(item, 'continuationEndpoint')]
+                    if action['targetId'].startswith('comment-replies-item') and 'continuationItemRenderer' in item:
+                        # Process the 'Show more replies' button
+                        continuations.append(next(self.search_dict(item, 'buttonRenderer'))['command'])
+
+            for comment in reversed(list(self.search_dict(response, 'commentRenderer'))):
+                time_text = comment['publishedTimeText']['runs'][0]['text']
+                time_parsed = dateparser.parse(time_text.split('(')[0].strip()).timestamp()
+                yield {'cid': comment['commentId'],
+                       'text': ''.join([c['text'] for c in comment['contentText'].get('runs', [])]),
+                       'time': time_text,
+                       'time_parsed': time_parsed,
+                       'author': comment.get('authorText', {}).get('simpleText', ''),
+                       'channel': comment['authorEndpoint']['browseEndpoint'].get('browseId', ''),
+                       'votes': comment.get('voteCount', {}).get('simpleText', '0'),
+                       'photo': comment['authorThumbnail']['thumbnails'][-1]['url'],
+                       'heart': next(self.search_dict(comment, 'isHearted'), False)}
+
             time.sleep(sleep)
 
+    @staticmethod
+    def regex_search(text, pattern, group=1, default=None):
+        match = re.search(pattern, text)
+        return match.group(group) if match else default
 
-def download_comments(youtube_id, sort_by=SORT_BY_RECENT, language=None, sleep=.1):
-    session = requests.Session()
-    session.headers['User-Agent'] = USER_AGENT
-
-    response = session.get(YOUTUBE_VIDEO_URL.format(youtube_id=youtube_id))
-
-    if 'uxe=' in response.request.url:
-        session.cookies.set('CONSENT', 'YES+cb', domain='.youtube.com')
-        response = session.get(YOUTUBE_VIDEO_URL.format(youtube_id=youtube_id))
-
-    html = response.text
-    ytcfg = json.loads(regex_search(html, YT_CFG_RE, default=''))
-    if not ytcfg:
-        return # Unable to extract configuration
-    if language:
-        ytcfg['INNERTUBE_CONTEXT']['client']['hl'] = language
-
-    data = json.loads(regex_search(html, YT_INITIAL_DATA_RE, default=''))
-
-    section = next(search_dict(data, 'itemSectionRenderer'), None)
-    renderer = next(search_dict(section, 'continuationItemRenderer'), None) if section else None
-    if not renderer:
-        # Comments disabled?
-        return
-
-    needs_sorting = sort_by != SORT_BY_POPULAR
-    continuations = [renderer['continuationEndpoint']]
-    while continuations:
-        continuation = continuations.pop()
-        response = ajax_request(session, continuation, ytcfg)
-
-        if not response:
-            break
-        if list(search_dict(response, 'externalErrorMessage')):
-            raise RuntimeError('Error returned from server: ' + next(search_dict(response, 'externalErrorMessage')))
-
-        if needs_sorting:
-            sort_menu = next(search_dict(response, 'sortFilterSubMenuRenderer'), {}).get('subMenuItems', [])
-            if sort_by < len(sort_menu):
-                continuations = [sort_menu[sort_by]['serviceEndpoint']]
-                needs_sorting = False
-                continue
-            raise RuntimeError('Failed to set sorting')
-
-        actions = list(search_dict(response, 'reloadContinuationItemsCommand')) + \
-                  list(search_dict(response, 'appendContinuationItemsAction'))
-        for action in actions:
-            for item in action.get('continuationItems', []):
-                if action['targetId'] == 'comments-section':
-                    # Process continuations for comments and replies.
-                    continuations[:0] = [ep for ep in search_dict(item, 'continuationEndpoint')]
-                if action['targetId'].startswith('comment-replies-item') and 'continuationItemRenderer' in item:
-                    # Process the 'Show more replies' button
-                    continuations.append(next(search_dict(item, 'buttonRenderer'))['command'])
-
-        for comment in reversed(list(search_dict(response, 'commentRenderer'))):
-            yield {'cid': comment['commentId'],
-                   'text': ''.join([c['text'] for c in comment['contentText'].get('runs', [])]),
-                   'time': comment['publishedTimeText']['runs'][0]['text'],
-                   'author': comment.get('authorText', {}).get('simpleText', ''),
-                   'channel': comment['authorEndpoint']['browseEndpoint'].get('browseId', ''),
-                   'votes': comment.get('voteCount', {}).get('simpleText', '0'),
-                   'photo': comment['authorThumbnail']['thumbnails'][-1]['url'],
-                   'heart': next(search_dict(comment, 'isHearted'), False)}
-
-        time.sleep(sleep)
-
-
-def search_dict(partial, search_key):
-    stack = [partial]
-    while stack:
-        current_item = stack.pop()
-        if isinstance(current_item, dict):
-            for key, value in current_item.items():
-                if key == search_key:
-                    yield value
-                else:
+    @staticmethod
+    def search_dict(partial, search_key):
+        stack = [partial]
+        while stack:
+            current_item = stack.pop()
+            if isinstance(current_item, dict):
+                for key, value in current_item.items():
+                    if key == search_key:
+                        yield value
+                    else:
+                        stack.append(value)
+            elif isinstance(current_item, list):
+                for value in current_item:
                     stack.append(value)
-        elif isinstance(current_item, list):
-            for value in current_item:
-                stack.append(value)
 
 
 def main(argv = None):
@@ -154,12 +162,13 @@ def main(argv = None):
                 os.makedirs(outdir)
 
         print('Downloading Youtube comments for video:', youtube_id)
+        downloader = YoutubeCommentDownloader()
         count = 0
         with io.open(output, 'w', encoding='utf8') as fp:
             sys.stdout.write('Downloaded %d comment(s)\r' % count)
             sys.stdout.flush()
             start_time = time.time()
-            for comment in download_comments(youtube_id, args.sort, args.language):
+            for comment in downloader.get_comments(youtube_id, args.sort, args.language):
                 comment_json = json.dumps(comment, ensure_ascii=False)
                 print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
                 count += 1

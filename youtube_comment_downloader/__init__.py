56e8120a9e55d53131041916952e51aee3d1d271
Egbert Bouman <ebouman@gmail.com>
2022-02-14 18:40:55 +0100

    Move main to __init__.py

--- youtube_comment_downloader/__init__.py
+++ youtube_comment_downloader/__init__.py
@@ -1,1 +1,60 @@
-from .downloader import YoutubeCommentDownloader
+import argparse
+import io
+import json
+import os
+import sys
+import time
+
+from .downloader import YoutubeCommentDownloader, SORT_BY_RECENT
+
+
+def main(argv = None):
+    parser = argparse.ArgumentParser(add_help=False, description=('Download Youtube comments without using the Youtube API'))
+    parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
+    parser.add_argument('--youtubeid', '-y', help='ID of Youtube video for which to download the comments')
+    parser.add_argument('--output', '-o', help='Output filename (output format is line delimited JSON)')
+    parser.add_argument('--limit', '-l', type=int, help='Limit the number of comments')
+    parser.add_argument('--language', '-a', type=str, default=None, help='Language for Youtube generated text (e.g. en)')
+    parser.add_argument('--sort', '-s', type=int, default=SORT_BY_RECENT,
+                        help='Whether to download popular (0) or recent comments (1). Defaults to 1')
+
+    try:
+        args = parser.parse_args() if argv is None else parser.parse_args(argv)
+
+        youtube_id = args.youtubeid
+        output = args.output
+        limit = args.limit
+
+        if not youtube_id or not output:
+            parser.print_usage()
+            raise ValueError('you need to specify a Youtube ID and an output filename')
+
+        if os.sep in output:
+            outdir = os.path.dirname(output)
+            if not os.path.exists(outdir):
+                os.makedirs(outdir)
+
+        print('Downloading Youtube comments for video:', youtube_id)
+        downloader = YoutubeCommentDownloader()
+        count = 0
+        with io.open(output, 'w', encoding='utf8') as fp:
+            sys.stdout.write('Downloaded %d comment(s)\r' % count)
+            sys.stdout.flush()
+            start_time = time.time()
+            for comment in downloader.get_comments(youtube_id, args.sort, args.language):
+                comment_json = json.dumps(comment, ensure_ascii=False)
+                print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
+                count += 1
+                sys.stdout.write('Downloaded %d comment(s)\r' % count)
+                sys.stdout.flush()
+                if limit and count >= limit:
+                    break
+        print('\n[{:.2f} seconds] Done!'.format(time.time() - start_time))
+
+    except Exception as e:
+        print('Error:', str(e))
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
